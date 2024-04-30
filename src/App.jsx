026aca497ae438a5723c2713cb0880f7061db317
Md.Arif <mohammadarif.ma02@gmail.com>
2024-05-01 02:05:31 +0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <h1 className="text-3xl text-red-400 font-bold underline">
+      Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
